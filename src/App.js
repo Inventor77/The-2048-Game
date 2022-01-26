52fabc37bdd24b1d9e5e53927b61c16da6cb0d65
Shreedutt <shreedutt77@gmail.com>
2022-01-27 02:56:27 +0530

    The Header Component has been styled.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Footer from "./Component/Footer";
 import GameBoard from "./Component/GameBoard";
 
 function App() {
-  const [sidelength, setSideLength] = useState(4);
-  const changeBoardSideLength = () => {
-    setSideLength((prevState) => prevState + 4);
-  };
+	const [sidelength, setSideLength] = useState(4);
+	const changeBoardSideLength = () => {
+		setSideLength((prevState) => prevState + 4);
+	};
 
-  return (
-    <div className="App">
-      <Header changeSideLength={changeBoardSideLength} />
-      <GameBoard sideLength={sidelength} />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="game_container">
+				<Header changeSideLength={changeBoardSideLength} />
+				<GameBoard sideLength={sidelength} />
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,37 +2,38 @@ import React from "react";
 import { HiOutlineArrowNarrowRight } from "react-icons/hi";
 
 function Header() {
-  return (
-    <header>
-      <div className="header_left">
-        <div className="header_title">2048</div>
-        <div className="header_subtitle">
-          <div>
-            Join the tiles, get to
-            <span className="highlight_text"> 2048 </span>
-          </div>
-          <div className="game_rules highlight_text">
-            How to play <HiOutlineArrowNarrowRight />{" "}
-          </div>
-        </div>
-      </div>
-      <div className="header_right">
-        <div className="score_board">
-          <div className="score">
-            SCORE
-            <span className="score_value"> 3100</span>
-          </div>
-          <div className="score">
-            BEST
-            <span className="score_value">3100 </span>
-          </div>
-        </div>
-        <div className="btn_container">
-          <button className="action_btn">New Game</button>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header>
+			<div className="header_left">
+				<div className="header_title highlight_text">2048</div>
+				<div className="header_subtitle">
+					<div>
+						Join the tiles, get to
+						<span className="highlight_text"> 2048! </span>
+					</div>
+					<div className="game_rules highlight_text">
+						How to play{" "}
+						<HiOutlineArrowNarrowRight style={{ fontWeight: "lighter" }} />
+					</div>
+				</div>
+			</div>
+			<div className="header_right">
+				<div className="score_board">
+					<div className="score">
+						<span className="score_head">SCORE</span>
+						<span className="score_value"> 0 </span>
+					</div>
+					<div className="score">
+						<span className="score_head">BEST</span>
+						<span className="score_value"> 3100 </span>
+					</div>
+				</div>
+				<div className="btn_container">
+					<button className="action_btn">New Game</button>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default Header;
